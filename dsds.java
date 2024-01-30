0e604c1891095b32382efb6e26fd74a995fd4bec
ravi sukhwal <113994509+Ravisukhwal76@users.noreply.github.com>
2024-01-30 04:23:33 -0800

    Update dsds.java

--- dsds.java
+++ dsds.java
@@ -1,1 +1,8 @@
+        const dateObject = formData.date instanceof Date ? formData.date : new Date(formData.date);
+        const dateString = dateObject.toISOString().split('T')[0];
+
+        const formDataString = {
+            ...formData,
+            date: dateString,
+        };
 
