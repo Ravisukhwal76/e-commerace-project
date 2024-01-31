9840737a4cedda9d725694e6f3b39723ba50e7a2
ravi sukhwal <113994509+Ravisukhwal76@users.noreply.github.com>
2024-01-30 21:18:09 -0800

    Update dsds.java

--- dsds.java
+++ dsds.java
@@ -1,8 +1,24 @@
-        const dateObject = formData.date instanceof Date ? formData.date : new Date(formData.date);
-        const dateString = dateObject.toISOString().split('T')[0];
+    const fetchData = async () => {
+            try {
+                const axiosInstance = axios.create({
+                    baseURL: 'http://localhost:8080',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${authToken}`, // Use Authorization header for token
+                    },
+                    withCredentials: true,
+                });
 
-        const formDataString = {
-            ...formData,
-            date: dateString,
+                const response = await axiosInstance.get(`/api/myTask/${id}`);
+                setTasks(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                console.log('Response:', error.response); // Log the response for more details
+                setError('Error fetching data. Please try again.');
+            } finally {
+                setLoading(false);
+            }
         };
 
+        fetchData();
+    }, []);
